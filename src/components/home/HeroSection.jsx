fafcd91ce7536cdfe86ea77cd96107c7d88f323c
kyl6682 <kyl6682@gmail.com>
2025-02-28 18:42:30 +0900

    [Home] HeroSection 반응형 구현

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,6 +1,7 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const HeaderSectionStyle = styled.section `
+const HeaderStyle = styled.section`
 background-color: #211C24;
 padding: 0 160px;
 color: #fff;
@@ -9,29 +10,72 @@ justify-content: space-between;
 align-items: center;
 `
 
-const WhiteStrokeButton = styled.button `
-padding: 16px 56px;
+const MobileHeaderStyle = styled(HeaderStyle)`
+background-color: #211C24;
+padding: 30px 160px;
+flex-direction: column;
+`
+
+const WhiteStrokeButton = styled.button`
+padding: 24px 56px;
 color: #fff;
 font-size: 16px;
 border: 1px solid #fff;
 border-radius: 6px;
 margin-top: 24px;
+
 `
 
-function HeroSection () {
-return (
-  <HeaderSectionStyle>
-    <div>
-      <h3 style={{fontSize: '25px', opacity : 0.4}}>Pro.Beyond.</h3>
-      <h1 style={{fontSize:'96px', fontWeight:300}}>iPhone 14 <span style={{fontWeight:900}}>Pro</span></h1>
-      <p style={{fontSize: '18px', color: '#909090'}}>Created to change everything for the better. For everyone</p>
-      <WhiteStrokeButton>Shop Now</WhiteStrokeButton>
-    </div>
-    <div>
-      <img src='./src/assets/Images/Iphone Image.png' />
-    </div>
-  </HeaderSectionStyle>
-)
+const HeroSectionBase = () => {
+    return (
+        <>
+            <div>
+                <h3 style={{ fontSize: '25px', opacity: 0.4 }}>Pro.Beyond.</h3>
+                <h1 style={{ fontSize: '96px', fontWeight: 300 }}>iPhone 14 <span style={{ fontWeight: 900 }}>Pro</span></h1>
+                <p style={{ fontSize: '18px', color: '#909090' }}>Created to change everything for the better. For everyone</p>
+                <WhiteStrokeButton>Shop Now</WhiteStrokeButton>
+            </div>
+            <div>
+                <img src='./src/assets/Images/Iphone Image.png' />
+            </div>
+        </>
+    )
+}
+
+const MobileHeroSection = () => {
+    return (
+        <MobileHeaderStyle>
+            <HeroSectionBase />
+        </MobileHeaderStyle>
+    )
+}
+
+const DesktopHeroSection = () => {
+    return (
+        <HeaderStyle>
+            <HeroSectionBase />
+        </HeaderStyle>
+    )
+}
+
+
+// 반응형 헤더 컴포넌트
+function HeroSection() {
+    const [isMobile, setIsMobile] = useState(typeof window !== "undefined" && window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    return isMobile ?
+        <MobileHeroSection /> : <DesktopHeroSection />;
 }
 
 export default HeroSection;
